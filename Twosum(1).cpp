00958dfe6d387a8b3c6146f35dd0fa023eba7d43
Saurabh <saurabhsalunke91@gmail.com>
2023-01-09 23:59:34 +0530

    DSA

--- Twosum(1).cpp
+++ Twosum(1).cpp
@@ -1,32 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-      
-        int i,j;
-        for(i=0;i<nums.size()-1;i++){
-            for(j=i+1;j<=nums.size()-1;j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            map<int,int> q;
+            for(int i=0;i<nums.size();i++){
+                int c=target-nums[i];
+                if(q.find(c)!=q.end()){
+                    return {i,q.find(c)->second};
                 }
+                q[nums[i]]=i;
             }
-        }
-        return {0,0};
-    
-    //     sort(nums.begin(),nums.end());
-    //     int i=0,j=nums.size()-1;
-    //     while(i<=j){
-    //         if((nums[i]+nums[j])>target){
-    //             j--;
-    //         }
-    //         else if((nums[i]+nums[j])<target){
-    //             i++;
-    //         }
-    //         else if((nums[i]+nums[j])==target){
-    //             break;
-    //         }
-    //     }
-    //    return {i,j};
+        return {0,0}; 
     }
 };
+    
---
+++ design_hashmap(706).cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<iostream>
+class MyHashMap {
+public:
+    map<int,int> q;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        q[key]=value;
+    }
+    
+    int get(int key) {
+        if(q.count(key)==1)
+            return q[key];
+        else
+            return -1;
+        
+    }
+    
+    void remove(int key) {
+        q.erase(key);
+    }
+};
